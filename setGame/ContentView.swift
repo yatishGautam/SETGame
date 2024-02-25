127d95a2a830b89f84f3e62bbfa7b2676d095ae3
yatishGautam <ydipankar@gmail.com>
2024-02-25 13:35:10 -0500

    Initial Commit

---
+++ setGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  setGame
+//
+//  Created by yatish gautam on 2024-02-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ setGame/setGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  setGameApp.swift
+//  setGame
+//
+//  Created by yatish gautam on 2024-02-25.
+//
+
+import SwiftUI
+
+@main
+struct setGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
