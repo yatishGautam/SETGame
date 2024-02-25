780246328af03d7268f2c3e96f459947dc14554e
yatishGautam <ydipankar@gmail.com>
2024-02-25 13:45:41 -0500

    changes

---
+++ setGame/Model/SetGame.swift
@@ -0,0 +1,39 @@
+//
+//  SetGame.swift
+//  setGame
+//
+//  Created by yatish gautam on 2024-02-25.
+//
+
+import Foundation
+struct SetGame<CardData>{
+    
+}
+
+struct card{
+    
+}
+
+enum shading {
+    case solid
+    case striped
+    case open
+}
+
+enum numberOfItems{
+    case one
+    case two
+    case three
+}
+
+enum cardColor{
+    case red
+    case green
+    case purple
+}
+
+enum cardSymbol{
+    case oval
+    case squiggles
+    case diamond
+}
